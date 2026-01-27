a4942d133b329d8ba76fb1f36c7a29b810fe56b7
Tanishk Bhatt <tanishkbhatt25@gmail.com>
2026-01-27 06:35:39 -0800

    Update GAME3.py
    Added functionality to play multiple rounds of the slot game and calculate total winnings.

--- GAME3.py
+++ GAME3.py
@@ -1,5 +1,6 @@
 # SLOTING MACHINE BETTING GAME
 import random
+import time
 
 def slotMatrix(betAmount: float) -> tuple[list[list], int]:
     marks = ["A", "B", "C"]
@@ -25,12 +26,23 @@ def slotMatrix(betAmount: float) -> tuple[list[list], int]:
 print("\nSLOTING MACHINE BETTING GAME!\n")
 
 betAmount = float(input("ENTER YOU BETTING AMOUNT : $"))
-res = slotMatrix(betAmount)
+rounds = int(input("ENTER THE NUMBEROF ROUNDS YOU WANNA PLAY : "))
 
-print("\nSLOT MATRIX :")
-for row in res[0]:
-    print(row)
+total_winnings = 0
 
-print(f"\nTOTAL WINNINGS : {res[1]}")
-print(f"TOTAL PROFIT : {res[2] - betAmount}")
-print(f"FINAL AMOUNT RECIEVED : {res[2]}\n")
+for i in range(rounds):
+    res = slotMatrix(betAmount / rounds)
+
+    print("\nSLOT MATRIX :")
+    for row in res[0]:
+        print(row)
+
+    print(f"\nTOTAL WINNINGS : {res[1]}")
+    print(f"TOTAL PROFIT (THIS ROUND): {res[2] - (betAmount/rounds)}")
+    print(f"FINAL AMOUNT RECIEVED : {res[2]}\n") 
+
+    total_winnings += res[2]
+    time.sleep(2)
+
+print(f"\nYOUR TOTAL WINNINGS AFTER {rounds} ROUNDS ON ${betAmount} IS ${total_winnings}")
+print(f"THATS {((total_winnings - betAmount) / betAmount) * 100}% PROFIT!\n") 
